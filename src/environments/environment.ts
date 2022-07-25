fa55cf38fe9b63184f1e4baa73f4e3fc93a09011
mbhossain <king.cseiu@gmail.com>
2022-07-25 17:57:25 +0600

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'adcf42e906msh822d91aec7c0cf6p13d17cjsndd8381447fac'
+
 };
 
 /*
